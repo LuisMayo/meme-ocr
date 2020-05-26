740b6c054eadaf9a93760f86734fb71707889839
LuisMayo <luismayovalbuena@outlook.es>
2020-05-26 22:09:44 +0200

    Attempt at base64

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask
 from flask import request
 from memeocr import MemeOCR
+import base64
 import wget
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    url = request.args.get('imageurl', '')
-    name = wget.download(url)
+    url = request.args.get('imageurl', None)
+    if url != None:
+        name = wget.download(url)
+    else:
+        # image_64_encode = request.args.get('image64', '')
+        # image_64_decode = base64.b64decode(image_64_encode)
+        # image_result = open('decode', 'wb')
+        # image_result.write(image_64_decode)
+        # nparr = np.fromstring(encoded_data.decode('base64'), np.uint8)
+        # name = 'decode'
     ocr = MemeOCR()
     txt = ocr.recognize(name)
     return ''.join(txt)
